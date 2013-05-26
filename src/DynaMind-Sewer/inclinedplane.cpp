4622f745efbad58956532538247c424ab9073587
Christian Urich <christian.urich@gmail.com>
2013-05-26 14:01:27 +1000

    width and height

--- src/DynaMind-Sewer/inclinedplane.cpp
+++ src/DynaMind-Sewer/inclinedplane.cpp
@@ -23,12 +23,16 @@ InclinedPlane::InclinedPlane()
 
 void InclinedPlane::run()
 {
-   DM::RasterData * plane = this->getRasterData("city", v_plane);
-   plane->setSize(width, height, cellsize, cellsize, 0, 0);
+    DM::RasterData * plane = this->getRasterData("city", v_plane);
+    plane->setSize(width, height, cellsize, cellsize, 0, 0);
 
-   for (long x = 0; x < height; x++) {
-       for (long y = 0; y < width; y++) {
-           plane->setCell(x,y,x*cellsize*slope);
-       }
-   }
+    for (long x = 0; x < width; x++) {
+        for (long y = 0; y <height; y++) {
+            plane->setCell(x,y,x*cellsize*slope);
+        }
+    }
+    DM::Logger(DM::Standard) << plane->getCell(0,0);
+    DM::Logger(DM::Standard) << plane->getCell(0,height-1);
+    DM::Logger(DM::Standard) << plane->getCell(width-1,0);
+    DM::Logger(DM::Standard) << plane->getCell(width-1, height-1);
 }
