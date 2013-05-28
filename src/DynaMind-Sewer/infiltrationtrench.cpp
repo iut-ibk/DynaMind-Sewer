82f73f565a4360d81eb22c1fda58e3bf607ed697
Christian Urich <christian.urich@gmail.com>
2013-05-28 17:57:58 +1000

    bug fix in infiltration system

--- src/DynaMind-Sewer/infiltrationtrench.cpp
+++ src/DynaMind-Sewer/infiltrationtrench.cpp
@@ -40,14 +40,25 @@ void InfiltrationTrench::run() {
     foreach (std::string uuid, catchment_uuids) {
         DM::Face * catchment = city->getFace(uuid);
 
+        std::vector<DM::LinkAttribute> exinsting_infils = catchment->getAttribute("INFILTRATION_SYSTEM")->getLinks();
+        double already_treated = 0;
+        foreach (DM::LinkAttribute la,exinsting_infils) {
+            DM::Component * inf = city->getComponent(la.uuid);
+            already_treated+=inf->getAttribute("treated_area")->getDouble();
+        }
         double roof_area_tot = catchment->getAttribute("roof_area_treated")->getDouble();
+
+        double toTreat = roof_area_tot - already_treated;
+        //DM::Logger(DM::Standard) << roof_area_tot;
+        //DM::Logger(DM::Standard) << already_treated;
+
+        if (toTreat < 5) continue;
 
         double imp = catchment->getAttribute("Impervious")->getDouble();
         double avalible_space = (1.-imp) * catchment->getAttribute("area")->getDouble();
-        if (roof_area_tot < 5) continue;
 
         //Design Infitration System
-        double Qzu = roof_area_tot * R *(1.e-7);//m3/s
+        double Qzu = toTreat * R *(1.e-7);//m3/s
         double kf = 1.e-4;
         double h = 1;
         double D = this->D*60;
@@ -55,16 +66,15 @@ void InfiltrationTrench::run() {
         double As = Qzu /(h/D+0.5*kf);
 
         if(avalible_space < As){
-
             DM::Logger(DM::Warning) << "Not enough space can't place infil system";
         }
 
-        catchment->getAttribute("INFILTRATION_SYSTEM")->setLinks(std::vector<DM::LinkAttribute>());
+        //catchment->getAttribute("INFILTRATION_SYSTEM")->setLinks(std::vector<DM::LinkAttribute>());
 
         DM::Component * infiltration_system = new DM::Component();
         city->addComponent(infiltration_system, view_infitration_system);
-        DM::Logger(DM::Debug) << roof_area_tot;
-        infiltration_system->addAttribute("treated_area", roof_area_tot);
+        DM::Logger(DM::Debug) << toTreat;
+        infiltration_system->addAttribute("treated_area", toTreat);
         infiltration_system->addAttribute("area", As);
         infiltration_system->addAttribute("h", h);
         infiltration_system->addAttribute("kf", kf);
